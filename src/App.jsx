b24b9718953b1196cec3dd536692712dbff20a53
divyanshg <divyanshg809@gmail.com>
2022-09-26 11:40:09 +0530

    Removed Contexts

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,23 @@ import CodeEditor from './Components/CodeEditor'
 import Console from './Components/Console'
 
 import { Provider } from "react-redux";
-import { EditorProvider } from './Contexts/Editor';
-import { EditorsProvider } from './Contexts/Editors';
-
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <EditorsProvider>
-        <EditorProvider>
-          <div className="App flex flex-col flex-1 h-screen">
-            <Topnav />
-            <div className="flex flex-1 h-[100%]">
-              <div className="flex flex-row flex-1 h-full bg-[#12202e]">
-                <Sidebar />
-                <div className="flex flex-col flex-1">
-                  <CodeEditor />
-                  <Console />
-                </div>
-              </div>
+      <div className="App flex flex-col flex-1 h-screen">
+        <Topnav />
+        <div className="flex flex-1 h-[100%]">
+          <div className="flex flex-row flex-1 h-full bg-[#12202e]">
+            <Sidebar />
+            <div className="flex flex-col flex-1">
+              <CodeEditor />
+              <Console />
             </div>
           </div>
-        </EditorProvider>
-      </EditorsProvider>
+        </div>
+      </div>
     </Provider>
   );
 }
